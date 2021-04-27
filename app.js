fa8bb57cf1f73c817a749cf9c24b08dbf6fc7433
Suraj Govind <surajgovind332000@gmail.com>
2021-04-27 18:02:10 +0530

    added fetch function code, random dog img api works

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log('hello')
 
-const data = fetch('https://dog.ceo/api/breeds/image/random')
-              .then(function(response) {
-                return response.json()
-              }).then(function(jsonData) {
-                console.log(jsonData)
-              })
+const container = document.getElementById('image-container');
+
+function fetchDogPics() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(function(rawResponse) {
+    return rawResponse.json()
+  }).then(function(usableData) {
+    const image = document.createElement('img');
+    image.src = usableData.message
+    container.appendChild(image)
+  })
+}
